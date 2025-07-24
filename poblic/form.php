f96337fd3c066e978f182349491e63e30396f027
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-24 19:07:25 +0430

    l

--- poblic/form.php
+++ poblic/form.php
@@ -7,21 +7,22 @@
     <link rel="stylesheet" href="../src/output.css">
 </head>
 <body>
-    <div class="h-screen w-full  flex justify-center items-center overflow-y-scroll">
-        <form action="" class="h-[70%] rounded-2xl w-[40%] p-4 shadow-xl shadow-black border flex flex-col justify-between ">
+    <div class="h-screen w-full flex justify-center items-center overflow-y-scroll">
+        <form action="" class="h-[60%] rounded-2xl w-[40%] p-4 shadow-xl shadow-black border flex flex-col justify-between ">
             <div class="">
                 <label for="" class="font-bold">ID</label>
 
                 <input type="number" class="py-2 w-full border rounded-xl outline-0">
             </div>
             <div class="">
-                <label for="">NAME</label>
+                <label for="" class="font-bold">NAME</label>
                 <input type="text" class="py-2 w-full border rounded-xl outline-0">
             </div>
             <div class="">
-                <label for="">PRODUCT</label>
+                <label for="" class="font-bold">PRODUCT</label>
                 <input type="text" class="py-2 w-full border rounded-md outline-0">
             </div>
+            <button class="PY-2 PX-3 bg-gray-600 rounded-xl">SEND</button>
         </form>
     </div>
 </body>
